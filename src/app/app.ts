a0268b188ec991b46d9aabaebfd6aef2d9c33dd9
Mark Luigi <markluigi@yahoo.it>
2025-09-27 14:29:42 +0200

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,9 @@ import { Component, signal } from '@angular/core';
 import { TodoList } from "./components/todo-list/todo-list";
 import { AutoList } from "./components/auto-list/auto-list";
 
+// Creo un tipo di nome 'Section' usando un *union type*: può essere solo "todo" o "auto" o "carello" o null
+type Section = "todo" | "auto" | "carrello" | null;
+
 @Component({
   selector: 'app-root',
   imports: [TodoList, AutoList],
@@ -10,4 +13,30 @@ import { AutoList } from "./components/auto-list/auto-list";
 })
 export class App {
   protected readonly title = signal('ng-todo');
+
+
+  // metodo 1:
+  selectedSection: Section = "auto";
+
+  select(section: Section) {
+    this.selectedSection = section;
+  }
+
+  // metodo 2:
+
+  // Variabile che indica quale sezione è attiva: "todo" o "auto"
+  // selectedSection: "todo" | "auto" = "todo";
+
+  // Funzione per cambiare la sezione attiva
+  //select(section: "todo" | "auto") {
+  // 'section' è il parametro passato alla funzione, usa un *union type*: può essere solo "todo" o "auto"
+  // Indica quale sezione vogliamo attivare ("todo" o "auto")
+
+  // Aggiorna la proprietà 'selectedSection' della classe con il nuovo valore
+  // 'this.selectedSection' si riferisce alla variabile dichiarata nella classe
+  // this.selectedSection = section;
+
+  // Dopo questa riga, la variabile selectedSection contiene il valore passato
+  // Aggiorna la sezione attiva e Angular mostra automaticamente la parte di interfaccia corrispondente
+  //  }
 }
